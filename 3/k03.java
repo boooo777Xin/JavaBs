7172e96a118814b2cd55837e0e51fa85576fd9a3
MSI\Administrator <yotta713daz@gmail.com>
2024-06-04 10:29:06 +0900

    commit

--- 3/k03.java
+++ 3/k03.java
@@ -2,7 +2,7 @@ public class k03 {
 
     public static void main(String[] args) {
 
-        for (int i = 6; i > 5; i++) {
+        for (int i = 1; i < 5; i++) {
             System.out.println("吉武");
         }
 
@@ -20,5 +20,21 @@ public class k03 {
         for (int cnt = 1; cnt <= 20; cnt = cnt + 2) {
             System.out.println(cnt);
         }
+
+        // while文でカウントしてみた(前置判定)
+        System.out.println("whileStart");
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+
+        // do~while文でカウントしてみた(後置判定)
+        System.out.println("do~whileStart");
+        int j = 1;
+        do {
+            System.out.println(j);
+            j++;
+        } while (j <= 10);
     }
 }
